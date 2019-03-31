42375600967bcaf05ff8d191f0fd549f6320cac0
William Ughetta <wughetta@princeton.edu>
2019-03-30 20:09:49 -0400

    express conflicts with react index.html

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => res.send('hello, world'))
+app.get('/hello', (req, res) => res.send('hello, world'))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'findex.html'));
+});
 
 app.listen(port);
