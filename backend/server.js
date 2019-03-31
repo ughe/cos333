1991c2b7e95e428dfaddccd677c7668ded7da5f4
William Ughetta <wughetta@princeton.edu>
2019-03-30 20:17:06 -0400

    restored index.html

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 4000
 app.get('/hello', (req, res) => res.send('hello, world'))
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'findex.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port);
