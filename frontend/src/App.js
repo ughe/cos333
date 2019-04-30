5285f4f66156bead88c457a07b51f79c06be8ddd
Mike Hallee <mdhallee@princeton.edu>
2019-04-29 22:04:23 -0400

    router prep

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Route, Switch, BrowserRouter, Link, generatePath } from 'react-router-dom';
+import * as Yup from 'yup';
+import Location from 'react-app-location';
+
 import "./w3.css";
 import './App.css';
 import IdeaFeed from './components/IdeaFeed';
-import IdeaCard from './components/Card/Card'
-import NewPost from './components/NewPost';
-import SortBar from './components/SortBar';
-import Login from './components/Login';
+import IdeaCard from './components/Card/Card';
+import TopBar from './components/TopBar';
+
 import Helmet from 'react-helmet';
 
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 
-class App extends Component {
+
+
+//const wholeNbr = Yup.number().integer().positive();
+
+
+
+class App extends React.Component {
    constructor(props) {
-    super(props);
+
+    super(props)
     this.handleChange = this.handleChange.bind(this)
     this.state = {
       query: null,
@@ -22,8 +33,9 @@ class App extends Component {
   }
 
   handleChange = (event) => {
-    this.setState({ query: event.target.value });
+    this.setState({ query : event.target.value });
   }
+
 
   render() {
 
@@ -36,32 +48,7 @@ class App extends Component {
           <style>{'body { background-color: #D3D3D3; }'}</style>
         </Helmet>
 
-        {/* TOP BAR */}
-        <div>
-          <div className="w3-bar w3-white w3-wide w3-padding w3-card w3-large">
-            <a href="#home" className="w3-bar-item w3-button">Tiger<b>TEAMS</b></a>
-
-            <div className="w3-bar-item w3-hide-small w3-right">
-              <Login className="w3-bar-item w3-hide-small w3-right" />
-            </div>
-
-
-
-            <div className="w3-bar-item w3-hide-small w3-right">
-              <a href="#about" className="w3-bar-item w3-button">About</a>
-            </div>
-
-           
-
-
-            <form className="w3-bar-item search-container center">
-                <input id="search" type="text" id="search-bar" placeholder="..." onChange={this.handleChange}/>
-                <a href="#"><img className="search-icon" 
-                src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></a>
-            </form>
-
-          </div>
-        </div>
+        <TopBar search ={this.handleChange}/>
 
         <IdeaFeed query={this.state.query}/>
         
